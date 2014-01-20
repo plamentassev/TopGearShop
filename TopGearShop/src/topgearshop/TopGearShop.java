85f863704d3fb78b0699c9903f208dcf8e12bb68
Kleon Kita <>
2014-01-19 21:46:47 -0500

    Commit of the first NetBeans Default class for testing purposes.

---
+++ TopGearShop/src/topgearshop/TopGearShop.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package topgearshop;
+
+/**
+ *
+ * @author kleon
+ */
+public class TopGearShop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
